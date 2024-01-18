69b120cce6841693da34f662fde9ecea40f94572
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-19 02:10:11 +0330

    New navbar menu

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,3 @@
-import Navbar from "@/components/Navbar";
-
 export default function Home() {
-  return (
-    <div className="h-screen flex flex-col items-center pt-8">
-      <h1>This is a sample title</h1>
-      <Navbar />
-    </div>
-  );
+  return <div></div>;
 }
---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,35 @@
+export const menuItems = [
+  {
+    title: "Home",
+  },
+  {
+    title: "Services",
+    submenu: [
+      {
+        title: "Graphic Design",
+      },
+      {
+        title: "Web Development",
+        submenu: [
+          {
+            title: "Frontend",
+          },
+          {
+            title: "Backend",
+            submenu: [
+              {
+                title: "node.js",
+              },
+              {
+                title: "PHP",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        title: "Mobile Application Development",
+      },
+    ],
+  },
+];
