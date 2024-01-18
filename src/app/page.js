3f89f3486563c36c38c67315781793c290195bcb
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-19 02:24:33 +0330

    antd design

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,43 @@
+import { Menu } from "antd";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Menu
+        mode="horizontal"
+        items={[
+          {
+            label: "Fashion",
+            key: "Fashion",
+            children: [
+              {
+                label: "Shirts",
+                key: "Shirts",
+              },
+              {
+                label: "T-Shirts",
+                key: "T_Shirts",
+              },
+              {
+                label: "Pants",
+                key: "Pants",
+              },
+              {
+                label: "Hats",
+                key: "Hats",
+              },
+            ],
+          },
+          {
+            label: "Furniture",
+            key: "Furniture",
+          },
+          {
+            label: "Electronics",
+            key: "Electronics",
+          },
+        ]}
+      ></Menu>
     </div>
   );
 }
